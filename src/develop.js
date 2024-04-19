fb8b783556a0479eb06990f2ed3eceef429ad619
Shvme7 <spangbob1.0@gmail.com>
2024-04-20 00:34:33 +0200

    bug-fixed

--- src/develop.js
+++ src/develop.js
@@ -1,18 +1,12 @@
-function GetNumberSum(a, b) {
-    
+function GetNumberSum(a, b) {  
     if (typeof a !== 'number' || typeof b !== 'number') {
         return "Error: Cannot plus non number params"; 
     }
-
-    
     if (a < 0 || b < 0) {
         return 0;
-    }
-
-    
+    }    
     return a + b;
 }
-
 console.log(GetNumberSum(1, 2)); 
 console.log(GetNumberSum(-1, -2)); 
 console.log(GetNumberSum("not a number", false)); 
