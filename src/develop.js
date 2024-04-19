76a62a0e3d7a37fb8dec2835620be93ffe0f137a
Shvme7 <166847182+Shvme7@users.noreply.github.com>
2024-04-20 00:38:21 +0200

    [#4]: Let's get started: Start learning JS code (#13)

--- src/develop.js
+++ src/develop.js
@@ -1,3 +1,12 @@
-let a = 12;
-let b = a + 12;
-console.log(a+b);
+function GetNumberSum(a, b) {  
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return "Error: Cannot plus non number params"; 
+    }
+    if (a < 0 || b < 0) {
+        return 0;
+    }    
+    return a + b;
+}
+console.log(GetNumberSum(1, 2)); 
+console.log(GetNumberSum(-1, -2)); 
+console.log(GetNumberSum("not a number", false)); 
