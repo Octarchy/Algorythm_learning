0d6735a4285d7ca2002719545f16782660bc776c
Shvme7 <spangbob1.0@gmail.com>
2024-04-20 00:02:49 +0200

    added-sum-function

--- src/develop.js
+++ src/develop.js
@@ -1,2 +1,18 @@
-let a = 'Hope I got it right';
-console.log(a);
+function GetNumberSum(a, b) {
+    
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return "Error: Cannot plus non number params"; 
+    }
+
+    
+    if (a < 0 || b < 0) {
+        return 0;
+    }
+
+    
+    return a + b;
+}
+
+console.log(GetNumberSum(1, 2)); 
+console.log(GetNumberSum(-1, -2)); 
+console.log(GetNumberSum("not a number", false)); 
