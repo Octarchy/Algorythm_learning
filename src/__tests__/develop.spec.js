537a977b2ae71fb6f43fe47bd7f5e67229243a48
Shvme7 <spangbob1.0@gmail.com>
2024-04-22 20:32:23 +0200

    bug-has-been-fixed

--- src/__tests__/develop.spec.js
+++ src/__tests__/develop.spec.js
@@ -3,13 +3,13 @@ import GetNumberSum from '../develop.js'
 function test() {
     const testCases = [
       { input: { a: 1, b: 2 }, expected: 3 },
-      { input: { a: -2, b: 1 }, expected: -1 },
-      { input: { a: 0, b: 0 }, expected: 0 } 
+      { input: { a: -2, b: 1 }, expected: 0 },
+      { input: { a: 'not a number', b: 'number' }, expected: NaN } 
     ];
   
     const testResults = [];
   
-    testCases.forEach((testCase) => {
+    testCases.map((testCase) => {
       const result = GetNumberSum(testCase.input.a, testCase.input.b); 
       const isPassed = result === testCase.expected; 
       testResults.push(isPassed);
