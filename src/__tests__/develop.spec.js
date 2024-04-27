5cd2407dc63439d63b0189c446fbeb33e0f0aa2f
Shvme7 <spangbob1.0@gmail.com>
2024-04-27 14:06:16 +0200

    try-catc-block-broken

--- src/__tests__/develop.spec.js
+++ src/__tests__/develop.spec.js
@@ -1,17 +1,21 @@
 import GetNumberSum from '../develop.js'
-
 function test() {
-    const testCases = [
-      { input: { a: 1, b: 2 }, expected: 3 },
-      { input: { a: -2, b: 1 }, expected: 0 },
-      { input: { a: 'not a number', b: 'number' }, expected: "Error: Cannot plus non number params" } 
-    ];
-  
-    const testResults = testCases.map((testCase) => {
-      const result = GetNumberSum(testCase.input.a, testCase.input.b); 
-      const isPassed = result === testCase.expected; 
-      return isPassed;
-    });
+  const testCases = [
+    { input: { a: 1, b: 2 }, expected: 3 },
+    { input: { a: -2, b: 1 }, expected: 0 },
+    { input: { a: 'not a number', b: 'number' }, expected: "Error: Cannot plus non number params" } 
+  ];
+  let testResults
+  try {
+    testResults = testCases.map((testCase) => {
+    const result = GetNumberSum(testCase.input.a, testCase.input.b); 
+    const isPassed = result === testCase.expected; 
+    return isPassed;
+  });
+} catch (error) {
+  console.log('err')
+}
+
     return testResults;
   }
   const results = test();
