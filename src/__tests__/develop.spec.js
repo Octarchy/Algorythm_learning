d1b735eff9df4d70ab5b7cab326e8671ecd6c3fc
Shvme7 <spangbob1.0@gmail.com>
2024-05-04 17:56:42 +0200

    throw-method-added

--- src/__tests__/develop.spec.js
+++ src/__tests__/develop.spec.js
@@ -1,22 +1,23 @@
-import GetNumberSum from '../develop.js'
+import GetNumberSum from '../develop.js';
+
 function test() {
   const testCases = [
     { input: { a: 1, b: 2 }, expected: 3 },
-    { input: { a: -2, b: 1 }, expected: 0 },
+    { input: { a: -2, b: 1 }, expected: 12 },
     { input: { a: 'not a number', b: 'number' }, expected: "Error: Cannot plus non number params" } 
   ];
-  let testResults
-  testResults = testCases.map((testCase) => {
-  const results = GetNumberSum(testCase.input.a, testCase.input.b); 
-  const isPassed = results === testCase.expected; 
-  return isPassed;
-});
+  const testResults = testCases.map((testCase) => {
+    const results = GetNumberSum(testCase.input.a, testCase.input.b); 
+    const isPassed = results === testCase.expected; 
+    return isPassed;
+  });
   return testResults;
 }
-try{
-  if (results !== expected ) {
-    throw  Error("expexted !== result")};
-}catch(error){
-alert('something went wrong')
+try {
+  const results = test();
+  if (results.includes(false)) {
+    throw 'There is an error in the test results'
+  }
+} catch(error) {
+  console.error('Something went wrong:');
 }
-console.log(results);
