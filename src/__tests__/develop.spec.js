6c32c073259457722cd26256d3cf14168b7161f8
Shvme7 <spangbob1.0@gmail.com>
2024-05-10 19:01:02 +0200

    changeed

--- src/__tests__/develop.spec.js
+++ src/__tests__/develop.spec.js
@@ -3,7 +3,7 @@ import GetNumberSum from '../develop.js';
 function test() {
   const testCases = [
     { input: { a: 1, b: 2 }, expected: 3 },
-    { input: { a: -2, b: 1 }, expected: 12 },
+    { input: { a: -2, b: 1 }, expected: 0 },
     { input: { a: 'not a number', b: 'number' }, expected: "Error: Cannot plus non number params" } 
   ];
   const testResults = testCases.map((testCase) => (GetNumberSum(testCase.input.a, testCase.input.b) === testCase.expected 
@@ -15,6 +15,7 @@ try {
   if (results.some(result => !result)) {
     throw `There is an error in the test results: ${results.map(result => result)}`
   }
+  else console.log("SUCCESS")
 } catch(error) {
-  console.error(`Something went wrong: ${error}`);
+  console.error(`FAILS: ${error}`);
 }
